1033719069e1e80c5aab4d63f9a294cfe8fa81cc
yubinmimi <yubinmimi>
2024-04-15 14:05:03 +0000

    dac ingested

---
+++ download.py
@@ -0,0 +1,97 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # National Downloadable Files for Doctors and Clinicians
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Configuration
+
+# COMMAND ----------
+
+!pip install tqdm
+
+# COMMAND ----------
+
+import requests
+from pathlib import Path
+from tqdm import tqdm
+import datetime
+from dateutil.relativedelta import *
+import zipfile
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Doctors - National Downloadable Files
+
+# COMMAND ----------
+
+#url = "https://edit.pdc.cms.gov/sites/default/files/archive/Doctors%20and%20clinicians"
+url = "https://data.cms.gov/provider-data/sites/default/files/archive/Doctors%20and%20clinicians"
+volumepath_root = "/Volumes/mimi_ws_1/provdatacatalog/src"
+volumepath_zip = f"{volumepath_root}/zipfiles"
+retrieval_range = 40 # in months, we wanted to get the data starting 2021-01-01; monthly update started in 2021
+
+# COMMAND ----------
+
+ref_monthyear = datetime.datetime.now()
+files_to_download = []
+for mon_diff in range(0, retrieval_range): 
+    monthyear = (ref_monthyear - relativedelta(months=mon_diff)).strftime('%m_%Y')
+    files_to_download.append(f"doctors_and_clinicians_{monthyear}.zip")
+
+# COMMAND ----------
+
+def download_file(url, filename, folder):
+    # NOTE the stream=True parameter below
+    with requests.get(f"{url}", stream=True) as r:
+        r.raise_for_status()
+        with open(f"{folder}/{filename}", 'wb') as f:
+            for chunk in tqdm(r.iter_content(chunk_size=8192)): 
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                #if chunk: 
+                f.write(chunk)
+
+# COMMAND ----------
+
+# Display the zip file links
+for filename in files_to_download:
+    # Check if the file exists
+    if Path(f"{volumepath_zip}/{filename}").exists():
+        # print(f"{filename} exists, skipping...")
+        continue
+    else:
+        print(f"{filename} downloading...")
+        year = filename[-8:-4]
+        url_download = f"{url}/{year}/{filename}"
+        download_file(url_download, filename, volumepath_zip)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Unzip the downloaded files
+
+# COMMAND ----------
+
+files_downloaded = [x for x in Path(volumepath_zip).glob("doctors_and_clinicians*")]
+
+# COMMAND ----------
+
+for file_downloaded in files_downloaded:
+    with zipfile.ZipFile(file_downloaded, "r") as zip_ref:
+        year = file_downloaded.stem[-4:]
+        month = file_downloaded.stem[-7:-5]
+        for member in zip_ref.namelist():
+            if (not Path(f"{volumepath_root}/doctors_and_clinicians/{year}_{month}/{member}").exists()):
+                print(f"Extracting {member}...")
+                try: 
+                    zip_ref.extract(member, path=f"{volumepath_root}/doctors_and_clinicians/{year}_{month}")
+                except NotImplementedError:
+                    print(f"skipping {file_downloaded} due to an error...")
+
+# COMMAND ----------
+
+
---
+++ ingest_dac.py
@@ -0,0 +1,171 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Ingest the Doctors and Clinicians Files
+# MAGIC
+# MAGIC There are many files, but we focus on two:
+# MAGIC
+# MAGIC - DAC_NationalDownloadableFile.csv
+# MAGIC - Facility_Affiliation.csv
+
+# COMMAND ----------
+
+from pathlib import Path
+import re
+import csv
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import col, lit, to_date, regexp_replace
+from datetime import datetime
+from dateutil.parser import parse
+import pandas as pd
+
+path = "/Volumes/mimi_ws_1/provdatacatalog/src/doctors_and_clinicians" # where all the input files are located
+catalog = "mimi_ws_1" # delta table destination catalog
+schema = "provdatacatalog" # delta table destination schema
+
+
+# COMMAND ----------
+
+def change_header(header_org):
+    return [re.sub(r'\W+', '', column.lower().replace(' ','_'))
+            for column in header_org]
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## NDF (National Downloadable File)
+
+# COMMAND ----------
+
+tablename = "dac_ndf" # destination table
+
+# COMMAND ----------
+
+# We want to skip those files that are already in the delta tables.
+# We look up the table, and see if the files are already there or not.
+files_exist = {}
+writemode = "overwrite"
+if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
+    files_exist = set([row["_input_file_date"] 
+                   for row in 
+                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
+                            .select("_input_file_date")
+                            .distinct()
+                            .collect())])
+    writemode = "append"
+
+# COMMAND ----------
+
+files = []
+for folderpath in Path(f"{path}").glob("*"):
+    if folderpath.stem < '2023_01':
+        continue
+    for filepath in Path(f"{folderpath}").glob("DAC_NationalDownloadableFile*"):
+        year = folderpath.stem[:4]
+        month = folderpath.stem[5:]
+        dt = parse(f"{year}-{month}-01").date()
+        if dt not in files_exist:
+            files.append((dt, filepath))
+files = sorted(files, key=lambda x: x[0], reverse=True)
+
+# COMMAND ----------
+
+int_columns = {"grd_yr", "num_org_mem"}
+
+for item in files:
+    # each file is relatively big
+    # so we load the data using spark one by one just in case
+    # the size hits a single machine memory
+    df = (spark.read.format("csv")
+            .option("header", "true")
+            .load(str(item[1])))
+    header = []
+    for col_old, col_new in zip(df.columns, change_header(df.columns)):
+
+        header.append(col_new)
+        
+        if col_new in int_columns:
+            df = df.withColumn(col_new, regexp_replace(col(col_old), "[$,%]", "").cast("int"))
+        else:
+            df = df.withColumn(col_new, col(col_old))
+            
+    df = (df.select(*header)
+          .withColumn("_input_file_date", lit(item[0])))
+    
+    # Some of these are manually corrected above. However, just in case
+    # we make the mergeSchema option "true"
+    (df.write
+        .format('delta')
+        .mode(writemode)
+        .option("mergeSchema", "true")
+        .saveAsTable(f"{catalog}.{schema}.{tablename}"))
+    
+    writemode="append"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Facility Affiliations
+
+# COMMAND ----------
+
+tablename = "dac_fa" # destination table
+
+# COMMAND ----------
+
+# We want to skip those files that are already in the delta tables.
+# We look up the table, and see if the files are already there or not.
+files_exist = {}
+writemode = "overwrite"
+if spark.catalog.tableExists(f"{catalog}.{schema}.{tablename}"):
+    files_exist = set([row["_input_file_date"] 
+                   for row in 
+                   (spark.read.table(f"{catalog}.{schema}.{tablename}")
+                            .select("_input_file_date")
+                            .distinct()
+                            .collect())])
+    writemode = "append"
+
+# COMMAND ----------
+
+files = []
+for folderpath in Path(f"{path}").glob("*"):
+    if folderpath.stem < '2023_01':
+        continue
+    for filepath in Path(f"{folderpath}").glob("Facility_Affiliation*"):
+        year = folderpath.stem[:4]
+        month = folderpath.stem[5:]
+        dt = parse(f"{year}-{month}-01").date()
+        if dt not in files_exist:
+            files.append((dt, filepath))
+files = sorted(files, key=lambda x: x[0], reverse=True)
+
+# COMMAND ----------
+
+for item in files:
+    # each file is relatively big
+    # so we load the data using spark one by one just in case
+    # the size hits a single machine memory
+    df = (spark.read.format("csv")
+            .option("header", "true")
+            .load(str(item[1])))
+    header = []
+    for col_old, col_new in zip(df.columns, change_header(df.columns)):
+        header.append(col_new)
+        df = df.withColumn(col_new, col(col_old))
+            
+    df = (df.select(*header)
+          .withColumn("_input_file_date", lit(item[0])))
+    
+    # Some of these are manually corrected above. However, just in case
+    # we make the mergeSchema option "true"
+    (df.write
+        .format('delta')
+        .mode(writemode)
+        .option("mergeSchema", "true")
+        .saveAsTable(f"{catalog}.{schema}.{tablename}"))
+    
+    writemode="append"
+
+# COMMAND ----------
+
+
